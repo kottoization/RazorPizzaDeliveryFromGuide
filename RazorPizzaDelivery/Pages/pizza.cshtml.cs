87e6da6f53b7a79b1036df35a67d8c457adb7525
kottoization <96475096+kottoization@users.noreply.github.com>
2023-03-22 22:31:41 +0100

    new pizzas

--- RazorPizzaDelivery/Pages/pizza.cshtml.cs
+++ RazorPizzaDelivery/Pages/pizza.cshtml.cs
@@ -12,8 +12,9 @@ namespace RazorPizzaDelivery.Pages
             new PizzasModel(){Imagetitle="Margerita",PizzaName="Margerita",basePrice=2,TomatoSauce=true, Cheese=true, FinalPrice=4},
             new PizzasModel(){Imagetitle="Hawaiian",PizzaName="Hawaiian",basePrice=2,TomatoSauce=true, Cheese=true, Pinaple=true, Bacon = true, FinalPrice=6},
             new PizzasModel(){Imagetitle="MeatFeast",PizzaName="MeatFeast",basePrice=2,TomatoSauce=true, Cheese=true, Chicken=true, Bacon = true,Peperoni=true,Corn=true, FinalPrice=8},
-            new PizzasModel(){Imagetitle="Peperoni",PizzaName="Peperoni",basePrice=2,TomatoSauce=true, Cheese=true, Peperoni=true, Jalapeno=true, FinalPrice=6 }
-
+            new PizzasModel(){Imagetitle="Peperoni",PizzaName="Peperoni",basePrice=2,TomatoSauce=true, Cheese=true, Peperoni=true, Jalapeno=true, FinalPrice=6 },
+            new PizzasModel(){Imagetitle="Vegetarian",PizzaName="Vegetarian",basePrice=2,TomatoSauce=true, Cheese=true, Mushroom=true, Corn=true, FinalPrice=6 },
+            new PizzasModel(){Imagetitle="Vegetarian",PizzaName="ExtraVegetarian",basePrice=2,TomatoSauce=true, Cheese=true, Mushroom=true, Jalapeno=true, Corn=true, Onion=true, FinalPrice=8 }
 
         };
         public void OnGet()
