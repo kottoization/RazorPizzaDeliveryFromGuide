e0c0663477ccbb5c480b0cc4f29291442159c264
kottoization <96475096+kottoization@users.noreply.github.com>
2023-03-22 01:10:27 +0100

    Checkout simple fields

--- RazorPizzaDelivery/Pages/Checkout/Checkout.cshtml.cs
+++ RazorPizzaDelivery/Pages/Checkout/Checkout.cshtml.cs
@@ -3,8 +3,18 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPizzaDelivery.Pages.Checkout
 {
+    [BindProperties(SupportsGet = true)]
     public class CheckoutModel : PageModel
     {
+
+        private string pizzaName;
+        private float pizzaPrice;
+        private string imageTitle;
+
+        public string PizzaName { get => pizzaName; set => pizzaName = value; }
+        public float PizzaPrice { get => pizzaPrice; set => pizzaPrice = value; }
+        public string ImageTitle { get => imageTitle; set => imageTitle = value; }
+
         public void OnGet()
         {
         }
